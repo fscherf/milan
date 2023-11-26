ea1b2b56b07da6c65e85ec25a90118c3b99958d9
Florian Scherf <mail@florianscherf.de>
2023-11-26 18:46:24 +0100

    tests: setup screenshot tests
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

---
+++ tests/test_0102_screenshots.py
@@ -0,0 +1,49 @@
+import pytest
+
+
+@pytest.mark.parametrize('image_format', ['jpeg', 'png'])
+@pytest.mark.parametrize('image_size', ['0x0', '800x0', '0x800', '800x800'])
+@pytest.mark.parametrize('browser_name', ['chromium'])
+def test_screenshots(
+        browser_name,
+        image_size,
+        image_format,
+        milan_artifacts_directory,
+):
+
+    from milan.utils.media import Image
+    from milan import Chromium
+
+    browser_class = {
+        'chromium': Chromium,
+    }[browser_name]
+
+    width, height = (int(i) for i in image_size.split('x'))
+    image_path = f'{browser_name}-{width}x{height}.{image_format}'
+
+    with browser_class.start() as browser:
+        browser.navigate_to_test_application()
+        browser.animation = True
+        browser.resize(1280, 720)
+
+        browser.screenshot(
+            output_path=image_path,
+            width=width,
+            height=height,
+        )
+
+    # run image checks
+    image = Image(image_path)
+
+    assert image.size > 0
+    assert image_format in image.format
+
+    if width:
+        assert image.width == width
+
+    if height:
+        assert image.height == height
+
+    if not width and not height:
+        assert image.width == 1280
+        assert image.height == 720
