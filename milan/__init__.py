4b48cbdca544b46db90b706db45ca244a71bd2d4
Florian Scherf <mail@florianscherf.de>
2024-03-07 00:00:35 +0100

    WIP: add get_browser_by_name
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- milan/__init__.py
+++ milan/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from milan.chromium import Chromium  # NOQA
 from milan.firefox import Firefox  # NOQA
 from milan.webkit import Webkit  # NOQA
@@ -5,3 +7,28 @@ from milan.errors import *  # NOQA
 
 VERSION = (0, 0, 0)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
+
+BROWSER = {
+    'chromium': Chromium,
+    'chrome': Chromium,
+    'firefox': Firefox,
+    'webkit': Webkit,
+    'safari': Webkit,
+}
+
+logger = logging.getLogger('milan')
+
+
+def get_browser_by_name(name):
+    logging.debug('searching for a browser by name "%s"', name)
+
+    _name = name.strip().lower()
+
+    if _name not in BROWSER:
+        raise RuntimeError(f'No browser with name "{name}" found')
+
+    browser = BROWSER[_name]
+
+    logging.debug('%s found', browser)
+
+    return browser
