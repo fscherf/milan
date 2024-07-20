3d39a9be27a579e653f7e7d8a1bdf62ff70e2718
Florian Scherf <mail@florianscherf.de>
2024-07-20 21:30:07 +0200

    Revert "pip: use setuptools scm instead of hand crafted version numbers"
    Unfortunately, setuptools_scm is not practical when building the Docker
    container because then the whole project has to be present in the container.
    
    This reverts commit 8ea280790a54498f9ce6ded59da03bc50087db12.

--- milan/__init__.py
+++ milan/__init__.py
@@ -1,14 +1,13 @@
 import logging
 
 from milan.frontend.commands import FrontendError  # NOQA
-from milan._version import version_tuple, version
 from milan.chromium import Chromium  # NOQA
 from milan.firefox import Firefox  # NOQA
 from milan.webkit import Webkit  # NOQA
 from milan.errors import *  # NOQA
 
-VERSION = version_tuple
-VERSION_STRING = version
+VERSION = (0, 0, 0)
+VERSION_STRING = '.'.join(str(i) for i in VERSION)
 
 BROWSER = {
     'chromium': Chromium,
